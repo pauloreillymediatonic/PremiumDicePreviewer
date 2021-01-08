e68e1aed733eebe8378180a8dedc534fbe097d22
Vinicius Pereira <vinicius.pereira@mediatonic.co.uk>
2021-01-08 10:19:41 +0000

    Made the project be in the root folder and simplified UI a bit

---
+++ Assets/Scripts/UIToggle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIToggle : MonoBehaviour
+{
+    public RectTransform rectTransform;
+
+    public void Toggle()
+    {
+        rectTransform.gameObject.SetActive(!rectTransform.gameObject.activeSelf);
+    }
+}
